a6fbcced1d7d746da1e014fef21a8bb008cb17b8
Dan Frenette <paradox815@gmail.com>
2014-10-21 11:54:00 -0400

    Users can add tasks through the form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
+    @task = Task.new
+  end
+
+  def create
+    current_user.tasks.create(task_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    task_params = params.require(:task).permit(:title, :description)
   end
 end
