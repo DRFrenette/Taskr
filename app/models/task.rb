1bc465ab965b62cb6ab42a786a937514bc6e1322
Dan Frenette <paradox815@gmail.com>
2014-10-21 13:57:46 -0400

    Add validations and error messages to task creator

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  def self.incomplete
+    where(completed: false)
+  end
+   validates :title, presence: true
+   validates :description, presence: true
 end
