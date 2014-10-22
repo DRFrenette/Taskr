26731a4cb9b1ce7470aa2f349951320f3d6162c5
Dan Frenette <paradox815@gmail.com>
2014-10-22 16:23:50 -0400

    Delete tasks with ajax

---
+++ app/assets/javascripts/delete-task.js
@@ -0,0 +1,16 @@
+$(function() {
+  var deleteTaskForm = $("form.delete");
+  deleteTaskForm.submit(function() {
+    var conversation = $.ajax({
+      type: "DELETE", 
+      url: $(this).attr("action"),
+      data: $(this).serialize()
+    });
+    conversation.done(function(deletedTask){
+      $(".task_" + deletedTask.id).fadeOut();
+    });
+    return false;
+  });
+});
+
+
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.find(params[:id])
     @task.destroy
 
-    redirect_to tasks_path
+    render json: { id: @task.id }
   end
 
   def update
